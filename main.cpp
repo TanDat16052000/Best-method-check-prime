51a63517808d81f54adda1e98f357a493a26bb0c
TanDat16052000 <62648452+TanDat16052000@users.noreply.github.com>
2020-11-09 22:08:10 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@
 #include<math.h>
 using namespace std;
 
-bool isPrime23(int value) {
+bool isPrime(int value) {
     if (value == 2 || value == 3) return true;
     if (value < 2 || value % 2 == 0 || value % 3 == 0) return false;
     int square_rootValue = (int) sqrt(value);
@@ -25,25 +25,8 @@ bool isPrime23(int value) {
     return true;
 }
 
-bool isPrime235(int value) {
-    if (value == 2 || value == 3 || value == 5) return true;
-    if (value < 2 || value % 2 == 0 || value % 3 == 0 || value % 5 == 0) return false;
-    int square_rootValue = (int) sqrt(value);
-    for (int i = 5; i <= square_rootValue - 2; i += 30) {
-        if (value % (i + 2) == 0)return false;
-        if (value % (i + 6) == 0)return false;
-        if (value % (i + 8) == 0)return false;
-        if (value % (i + 12) == 0)return false;
-        if (value % (i + 14) == 0)return false;
-        if (value % (i + 18) == 0)return false;
-        if (value % (i + 24) == 0)return false;
-        if (value % (i + 26) == 0)return false;
-    }
-    return true;
-}
-
 int main(int argc, char** argv) {
     for (int i = 0; i < 100; i++)
-        if (isPrime23(i))cout << i << " ";
+        if (isPrime(i))cout << i << " ";
     return 0;
 }
