69a169b8e35e425c70c8156eac2ab8d056a979c3
TanDat16052000 <62648452+TanDat16052000@users.noreply.github.com>
2021-04-06 22:47:02 +0700

    init

---
+++ cmake-build-debug/CMakeFiles/Best_method_check_prime.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Best_method_check_prime.exe"
+  "Best_method_check_prime.exe.manifest"
+  "Best_method_check_prime.pdb"
+  "CMakeFiles/Best_method_check_prime.dir/main.cpp.obj"
+  "libBest_method_check_prime.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Best_method_check_prime.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
